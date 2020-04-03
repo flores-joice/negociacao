af88a3bc69ecedea29db2a8f56f0793c5865c6ae
flores-joice <flores.joice@gmail.com>
2020-04-03 19:16:51 -0300

    criando class Negociacao

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,33 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        object.freezer(this);
+
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+
+        return this._data;
+
+    }
+
+    get quantidade() {
+
+        return this._quantidade;
+
+    }
+
+    get valor() {
+
+        return this._valor;
+
+    }
+}
