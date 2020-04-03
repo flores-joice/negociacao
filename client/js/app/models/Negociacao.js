77dfbb99d3a04cc1f8222c8a60f8cbbd15b52db0
flores-joice <flores.joice@gmail.com>
2020-04-03 20:51:38 -0300

    fazendo ligação entre as ações do usuário e o modelo

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -5,7 +5,7 @@ class Negociacao {
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
-        object.freezer(this);
+        Object.freeze(this);
 
     }
 
