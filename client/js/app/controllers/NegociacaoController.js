3cb1539b311e241990206bb3983b9d3fd9e36b59
flores-joice <flores.joice@gmail.com>
2020-04-28 18:22:30 -0300

    blidando criação de negociações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,25 +1,32 @@
 class NegociacaoController {
     
     constructor() {
-
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
-
-        let helper = new DateHelper();
         
-        let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    };
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-          );
-        
-          console.log(negociacao);
-          console.log(helper.dataParaTexto(negociacao.data));
-    };
+        );
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
 };  
